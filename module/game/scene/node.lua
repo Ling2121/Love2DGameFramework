dea0c16e5154f15ab790c2f51261983ce7c6950d
Ling_2121 <2609344951@qq.com>
2019-03-12 20:17:27 +0800

    改进场景系统中

--- module/game/scene/node.lua
+++ module/game/scene/node.lua
@@ -3,7 +3,6 @@ local depth_node = require"module/struct/depth_list/node"
 local node = class("scene_node",depth_node,signal){
     __node_name = nil,
     __at_scene = nil,
-    __view_id = 1,--1:摄像机 2:屏幕
     __wait = false,
 }
 
@@ -28,13 +27,8 @@ function node:set_node_name(name)
     return self
 end
 
-function node:set_view(id)
-    self.__view_id = id or 1
-    return self
-end
-
 function node:get_node(name)
-    if name then
+    if name and self.__at_scene then
         return self.__at_scene:get_node(name)
     end
 end
---
+++ module/game/scene/node2d.lua
@@ -0,0 +1,38 @@
+local node = require"module/game/scene/node"
+
+local node2d = class("node2d",node){
+    x = 0,
+    y = 0,
+    __view_id = 1,--1:摄像机 2:屏幕
+}
+
+function node2d:set_view(id)
+    self.__view_id = id or 1
+    return self
+end
+
+function node2d:set_pos(x,y)
+    self.x = x
+    self.y = y
+end
+
+function node2d:move_pos(x,y)
+    self.x = self.x + x
+    self.y = self.y + y
+end
+
+function node2d:get_pos()
+    return self.x,self.y
+end
+
+function node2d:get_wpos()--获取世界坐标，位于场景中才有效
+    if self.__view_id == 1 then
+        local cam = self:get_node("camera")
+        if cam then
+            return cam:to_world_pos(self.x,self.y)
+        end
+    end
+    return self.x,self.y
+end
+
+return node2d
