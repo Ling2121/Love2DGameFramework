34d0788d5b13a541384e6f7c3af310a2b04901bd
Ling_2121 <2609344951@qq.com>
2019-03-14 13:55:24 +0800

    ...

--- misc/area/signal/signal_proxy.lua
+++ misc/area/signal/signal_proxy.lua
@@ -1,10 +1,22 @@
 local signal_proxy = class("signal_proxy"){
+    __all_connect = {},
     __agent = nil,--代理人
     __proxy = nil,--代理对象
+    __all_proxy = {},
 }
 
 function signal_proxy:__init(agent,porxy)
     self.__agent = agent
-    self.__proxy = porxy
+    self.__proxy = porxy--可以是函数 proxy = __porxy(agent)
 end
+
+function signal_proxy:add_proxy(proxy_name)
+    
+end
+
+function signal_proxy:connect(conn_obj,sig_name,conn_func_name)
+
+end
+
+return signal_proxy
 
